05f8c034add5c796ab45a2d979f1b9c4af9f2448
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2025-03-29 05:26:02 +0100

    Feat: Depreciate 'api/v1/users/create' route. Use 'api/v1/signup' instead

--- src/app/api/v1/users/create/route.ts
+++ src/app/api/v1/users/create/route.ts
@@ -1,41 +1,46 @@
 import prisma from "@/app/lib/prisma";
 import ErrorHandler from "@/app/lib/ErrorHandler";
 import { NextResponse } from "next/server";
-// import { Request, Response } from "express";
+import { hashPassword } from "@/app/lib/auth/password";
 
 export async function POST(request: Request): Promise<Response> {
-    try {
-        // Parse the request body as JSON
-        const body = await request.json();
-    
-        // Check if the request body is empty
-        if (!body) {
-          return NextResponse.json(
-            { message: "Data not sent in request." },
-            { status: 400 }
-          );
-        }
+  try {
+    // Parse the request body as JSON
+    const body = await request.json();
 
-        if (!body.email || !body.phone || !body.username || !body.hashedPassword) {
-            return NextResponse.json(
-              { message: "Required fields missing." },
-              { status: 400 }
-            );
-        }
-
-        const newUser = await prisma.user.create({
-            data: {
-                email: body.email,
-                phone: body.phone,
-                username: body.username,
-                hashedPassword: body.hashedPassword,
-            }
-        });
-
-        console.log(newUser);
-        return NextResponse.json({ message: "Data received.", data: newUser }, { status: 200 });
-    } catch(error){
-        const { status, message } = ErrorHandler(error);
-        return NextResponse.json({ message: message }, { status });
+    // Check if the request body is empty
+    if (!body) {
+      return NextResponse.json(
+        { message: "Data not sent in request." },
+        { status: 400 }
+      );
     }
+
+    if (!body.email || !body.phone || !body.username || !body.password) {
+      return NextResponse.json(
+        { message: "Required fields missing." },
+        { status: 400 }
+      );
+    }
+
+    if (body.password) {
+      body.hashedPassword = await hashPassword(body.password);
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: body.email,
+        phone: body.phone,
+        username: body.username,
+        hashedPassword: body.hashedPassword,
+      }
+    });
+
+    console.log(newUser);
+    return NextResponse.json({ message: "Data received.", data: newUser }, { status: 200 });
+  } catch (error) {
+    const { status, message } = ErrorHandler(error);
+    return NextResponse.json({ message: message }, { status });
+  }
 }
+// DEPRECIATED //
